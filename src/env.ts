adc34d14357501aa2e03ca8739b841a736248b5b
Deyvid-22 <silvadeyvid017@gmail.com>
2025-02-18 13:30:00 -0300

    feat: configure redis connection and update docker-compose.yml for redis service

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,11 @@ import { z } from "zod";
 
 const envShema = z.object({
   PORT: z.coerce.number().default(3333),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
 });
 
 export const env = envShema.parse(process.env);
